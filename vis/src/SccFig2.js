18c6e9072bcafcabc087fff07f5908782b68f116
Marcos Ojeda <subliminal@gmail.com>
2018-08-26 09:12:17 -0700

    add rough fig2

---
+++ vis/src/SccFig2.js
@@ -0,0 +1,11 @@
+// @flow
+
+import * as React from 'react';
+
+import { geoMercator, geoPath } from "d3-geo"
+import { feature } from "topojson-client"
+
+class Figure2 extends React.Component {
+  render() {
+  }
+}
