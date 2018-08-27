9f857266d33c0d5eadb7cacfd11b21e9efa2de35
Marcos Ojeda <subliminal@gmail.com>
2018-08-26 21:52:39 -0700

    some visualization updates to fig2

--- vis/src/App.js
+++ vis/src/App.js
@@ -2,25 +2,26 @@
 import React, { Component } from "react";
 
 import SCCFigure from "./SccFig1";
-import SCCFig2 from './SccFig2';
+import SCCFig2, { Fig2Options } from "./SccFig2";
 
 import Papa from "papaparse";
 
 import "./App.css";
 
-const fig1Filter = country => row => row.eta === '1p5' && row.prtp === '2' && row.ISO3 === country;
+const fig1Filter = country => row =>
+  row.eta === "1p5" && row.prtp === "2" && row.ISO3 === country;
 
 const fetchCountryFig1 = (countryName, filterFn) => {
-  const data = [];  
+  const data = [];
   const test = fig1Filter(countryName);
   Papa.parse(`${process.env.PUBLIC_URL}/cscc_v1.csv`, {
     download: true,
     header: true,
-    dynamicTyping: (name) => ['16.7%', '50%', '83.3%'].includes(name),
+    dynamicTyping: name => ["16.7%", "50%", "83.3%"].includes(name),
     step: (results, parser) => {
       const row = results.data[0];
       if (test(row)) {
-        data.push(row)
+        data.push(row);
       }
     },
     complete: () => console.log(data)
@@ -31,7 +32,7 @@ class App extends Component {
   state = { country: "WLD" };
 
   componentDidMount() {
-    fetchCountryFig1('USA');
+    // fetchCountryFig1('USA');
   }
 
   render() {
@@ -42,32 +43,35 @@ class App extends Component {
     };
     return (
       <div className="App">
-        <button
-          onClick={() => this.setState({ country: "WLD" })}
-          className={this.state.country === "WLD" ? "active" : undefined}
-        >
-          world
-        </button>
-        <button
-          onClick={() => this.setState({ country: "USA" })}
-          className={this.state.country === "USA" ? "active" : undefined}
-        >
-          usa
-        </button>
-        <button
-          onClick={() => this.setState({ country: "RUS" })}
-          className={this.state.country === "RUS" ? "active" : undefined}
-        >
-          russia
-        </button>
         {false && (
-          <SCCFigure
-            data={allData[this.state.country].data}
-            clamp={allData[this.state.country].clampRight}
-            country={this.state.country}
-          />
+          <React.Fragment>
+            <button
+              onClick={() => this.setState({ country: "WLD" })}
+              className={this.state.country === "WLD" ? "active" : undefined}
+            >
+              world
+            </button>
+            <button
+              onClick={() => this.setState({ country: "USA" })}
+              className={this.state.country === "USA" ? "active" : undefined}
+            >
+              usa
+            </button>
+            <button
+              onClick={() => this.setState({ country: "RUS" })}
+              className={this.state.country === "RUS" ? "active" : undefined}
+            >
+              russia
+            </button>
+            <SCCFigure
+              data={allData[this.state.country].data}
+              clamp={allData[this.state.country].clampRight}
+              country={this.state.country}
+            />
+          </React.Fragment>
         )}
-        <SCCFig2 />
+
+        <Fig2Options />
       </div>
     );
   }
--- vis/src/SccFig2.js
+++ vis/src/SccFig2.js
@@ -9,33 +9,46 @@ import { schemeRdYlBu } from "d3-scale-chromatic";
 
 import Papa from "papaparse";
 
+const projection = geoNaturalEarth1()
+  .scale(100)
+  .translate([800 / 2, 450 / 2]);
+const worldMap = feature(map, map.objects.countries).features;
+
 type Props = {
   data: Array<{}>,
   ssp?: string,
   rcp?: string,
-  dmg?: string
+  dmg?: string,
+  onCountryEnter: (name: string, data: {}) => any
 };
 
 class Figure2 extends React.Component<Props> {
   render() {
     const { data } = this.props;
-    const worldMap = feature(map, map.objects.countries).features;
-    const projection = geoNaturalEarth1()
-      .scale(100)
-      .translate([800 / 2, 450 / 2]);
     return (
-      <svg width={800} height={450} viewBox="0 0 800 450">
+      <svg width={800} height={350} viewBox="150 0 800 350">
+        <g transform={`translate(675, 75)`}>>
+          <Fig2Legend bins={fig2Bins} labels={{ 0: "<-10", 7: ">100" }} />
+        </g>
         <g className="countries">
-          {worldMap.map((country, index) => {
-            // console.log(data)
-            // access metadata via data.properties
+          {worldMap.map(country => {
+            // access feature metadata via country.properties
+            const {
+              properties: { label, id }
+            } = country;
+            const countryRow = data.filter(row => row.ISO3 === id)[0];
+
             return (
               <path
-                key={country.properties.id}
-                id={country.properties.id}
-                stroke="#bbb"
+                key={label}
+                label={label}
+                stroke="#ccc"
                 d={geoPath().projection(projection)(country)}
-                fill={colorFor(country.properties.id, data)}
+                fill={colorFor(countryRow)}
+                onMouseEnter={() =>
+                  this.props.onCountryEnter &&
+                  this.props.onCountryEnter(label, countryRow)
+                }
               />
             );
           })}
@@ -52,12 +65,14 @@ type CSVFig2Props = {
   csvPath?: string
 };
 type CSVFig2State = {
-  data: Array<{}>
+  data: Array<{}>,
+  loading: boolean
 };
 
-class CSVFig2 extends React.Component<CSVFig2Props, CSVFig2State> {
+class CSVFig2Loader extends React.PureComponent<CSVFig2Props, CSVFig2State> {
   state = {
-    data: []
+    data: [],
+    loading: false
   };
   static defaultProps = {
     csvPath: `${process.env.PUBLIC_URL}/cscc_v1.csv`,
@@ -66,11 +81,11 @@ class CSVFig2 extends React.Component<CSVFig2Props, CSVFig2State> {
     dmg: "bhm_sr",
     prtp: "2",
     eta: "1p5",
-    dmgfuncpar: 'bootstrap',
-    climate: 'uncertain'
+    dmgfuncpar: "bootstrap",
+    climate: "uncertain"
   };
 
-  componentDidMount() {
+  fetchData = () => {
     const data = [];
     const { ssp, rcp, dmg, prtp, eta, dmgfuncpar, climate } = this.props;
     const test = row =>
@@ -81,48 +96,210 @@ class CSVFig2 extends React.Component<CSVFig2Props, CSVFig2State> {
       row.eta === eta &&
       row.dmgfuncpar === dmgfuncpar &&
       row.climate === climate;
+    this.setState({ loading: true });
     Papa.parse(this.props.csvPath, {
       download: true,
       header: true,
       dynamicTyping: name => ["16.7%", "50%", "83.3%"].includes(name),
       step: (results, parser) => {
         const row = results.data[0];
-        // console.log(row);
         if (test(row)) {
-          console.log(row)
           data.push(row);
         }
       },
       complete: () => {
-        this.setState({ data });
+        this.setState({ data, loading: false });
       }
     });
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.ssp !== this.props.ssp ||
+      prevProps.rcp !== this.props.rcp ||
+      prevProps.dmg !== this.props.dmg
+    ) {
+      console.log(this.props);
+      this.fetchData();
+    }
   }
 
   render() {
-    console.log(this.state.data.map(r => ({iso: r.ISO3, median: r['50%']})))
-    return <Figure2 data={this.state.data} />;
+    const { loading, data } = this.state;
+    return this.props.children({ data, loading });
   }
 }
 
-const colorFor = (iso3, data) => {
-  const row = data.filter(row => row.ISO3 === iso3)[0];
+const colorFor = row => {
   if (row) {
     const index = binner(row["50%"], true);
     if (index > -1) {
-      return schemeRdYlBu[11][index+0];
+      return schemeRdYlBu[11][index + 0];
     }
   }
   return "#eee";
 };
 
-const binner = (val, invert=false) => {
-  const bins = [[-1000, -10], [-10, -1], [-1, 0], [0, 1], [1, 10], [10, 50], [50, 100], [100, 100000]];
+const fig2Bins = [
+  [-1000, -10],
+  [-10, -1],
+  [-1, 0],
+  [0, 1],
+  [1, 10],
+  [10, 50],
+  [50, 100],
+  [100, 100000]
+];
+const binner = (val, invert = false, bins = fig2Bins) => {
   const indices = bins.map(([min, max]) => val > min && val <= max);
   const pos = indices.indexOf(true);
-  return invert ? (bins.length - pos) : pos;
+  return invert ? bins.length - pos : pos;
 };
 
-export default CSVFig2;
+const Fig2Legend = ({ bins, labels, size = 15, x, y }) => {
+  const height = (size + 1) * bins.length;
+  return (
+      bins.map((bin, idx) => {
+        return (
+          <React.Fragment key={bin[0]}>
+            <rect
+              fill={colorFor({ "50%": (bin[0] + bin[1]) / 2 })}
+              width={size}
+              height={size}
+              x={0}
+              y={idx * (size + 1)}
+            />
+            <text
+              className="f2-legend-label"
+              x={size + 2}
+              y={idx * (size + 1) + 10}
+            >
+              {labels[idx] !== undefined
+                ? labels[idx]
+                : `${bin[0]} – ${bin[1]}`}
+            </text>
+          </React.Fragment>
+        );
+      })
+  );
+};
+
+type ParamProps = {};
+type ParamState = {
+  ssp: "SSP1" | "SSP2" | "SSP3" | "SSP4" | "SSP5",
+  rcp: "rcp45" | "rcp60" | "rcp85",
+  dmg: "bhm_sr" | "bhm_richpoor_sr" | "bhm_lr" | "bhm_richpoor_lr" | "djo",
+  hoveredName?: string,
+  hoveredData?: {}
+};
+
+export class Fig2Options extends React.Component<ParamProps, ParamState> {
+  state = {
+    ssp: "SSP2",
+    rcp: "rcp60",
+    dmg: "bhm_sr"
+  };
+  update = evt => {
+    this.setState({ [evt.currentTarget.name]: evt.currentTarget.value });
+  };
+  render() {
+    const ssps = ["SSP1", "SSP2", "SSP3", "SSP4", "SSP5"];
+    const rcps = ["rcp45", "rcp60", "rcp85"];
+    const dmgs = [
+      "bhm_sr",
+      "bhm_richpoor_sr",
+      "bhm_lr",
+      "bhm_richpoor_lr",
+      "djo"
+    ];
+
+    const { hoveredData } = this.state;
+
+    return (
+      <div>
+        <div>
+          <select name="ssp" onChange={this.update}>
+            {ssps.map(ssp => (
+              <option key={ssp} value={ssp}>
+                {ssp}
+              </option>
+            ))}
+          </select>
+          <select name="rcp" onChange={this.update}>
+            {rcps.map(ssp => (
+              <option key={ssp} value={ssp}>
+                {ssp}
+              </option>
+            ))}
+          </select>
+          <select name="dmg" onChange={this.update}>
+            {dmgs.map(ssp => (
+              <option key={ssp} value={ssp}>
+                {ssp}
+              </option>
+            ))}
+          </select>
+        </div>
+        <CSVFig2Loader
+          ssp={this.state.ssp}
+          dmg={this.state.dmg}
+          rcp={this.state.rcp}
+        >
+          {({ data, loading }) => (
+            <div className={loading ? "loading-map" : undefined}>
+              <Figure2 data={data} onCountryEnter={this.hoverCountry} />
+            </div>
+          )}
+        </CSVFig2Loader>
+
+        <div
+          className="f2-country-detail"
+          style={{ borderTop: `3px solid ${colorFor(hoveredData)}` }}
+        >
+          <p className="f2-countryname">
+            <strong>{this.state.hoveredName}</strong>
+          </p>
+          {hoveredData ? (
+            <React.Fragment>
+              <p>
+                <span className="f2-clscc">
+                  16.3%: {hoveredData && fmt(hoveredData["16.7%"])}
+                </span>
+                <span className="f2-clscc">
+                  50%: {hoveredData && fmt(hoveredData["50%"])}
+                </span>
+                <span className="f2-clscc">
+                  83.3%: {hoveredData && fmt(hoveredData["83.3%"])}
+                </span>
+              </p>
+              <p className="f2-clscc">
+                in USD/tCO
+                <sub>2</sub>
+              </p>
+            </React.Fragment>
+          ) : (
+            this.state.hoveredName && (
+              <p className="f2-nodata">No data available</p>
+            )
+          )}
+        </div>
+      </div>
+    );
+  }
+  hoverCountry = (hoveredName: string, hoveredData: {}) => {
+    this.setState({
+      hoveredName,
+      hoveredData
+    });
+  };
+}
+
+const fmt = val => typeof val === "number" && Math.floor(val * 1000) / 1000;
+
+export default Figure2;
 
 // rcp, ssp, dmdfn
