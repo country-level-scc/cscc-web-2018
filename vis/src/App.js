8adcf8bc04f68ba2b6561ec06bc29277e5ed7e97
Marcos Ojeda <subliminal@gmail.com>
2018-09-08 16:25:17 -0700

    clean up app.js, hide fig 4 for now

--- vis/src/App.js
+++ vis/src/App.js
@@ -1,25 +1,45 @@
 // @flow
 import React, { Component } from "react";
 
-import SCCFigure, {Fig1Options} from "./SccFig1";
-import SCCFig2, { Fig2Options } from "./SccFig2";
-import {CsccFig4} from './SccFig4.js';
+import { Fig1Options } from "./SccFig1";
+import { Fig2Options } from "./SccFig2";
+import { CsccFig4 } from "./SccFig4.js";
 
 import "./App.css";
 
-
 class App extends Component {
-
-  state={
-    selectedCountry: 'WLD',
-  }
+  state = {
+    selectedCountry: "WLD"
+  };
 
   render() {
     return (
       <div className="App">
-        <CsccFig4 data={[{ISO3: 'USA', shareEmissions: 15.6, shareScc: 11.7, sccPerCapita: 0.3, logGdp: 10}]}/>
-        <Fig2Options onCountrySelect={selectedCountry => this.setState({selectedCountry})} />
-        <Fig1Options country={this.state.selectedCountry} onCountryChange={selectedCountry => this.setState({selectedCountry})} />
+        {/* <CsccFig4
+          data={[
+            {
+              ISO3: "USA",
+              shareEmissions: 15.6,
+              shareScc: 11.7,
+              sccPerCapita: 0.3,
+              logGdp: 10
+            }
+          ]}
+        /> */}
+
+        <Fig2Options
+          onCountrySelect={selectedCountry =>
+            this.setState({
+              selectedCountry
+            })
+          }
+        />
+        <Fig1Options
+          country={this.state.selectedCountry}
+          onCountryChange={selectedCountry =>
+            this.setState({ selectedCountry })
+          }
+        />
       </div>
     );
   }
