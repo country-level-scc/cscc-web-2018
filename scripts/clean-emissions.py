a1da64fcae1b56cd7c97483d95d6624fd190dc29
Marcos Ojeda <subliminal@gmail.com>
2018-09-08 14:54:54 -0700

    clean emissions now emits a column for Emissions Share using WLD as total

--- scripts/clean-emissions.py
+++ scripts/clean-emissions.py
@@ -10,9 +10,26 @@ def extract_emissions(data):
   for col in cols_to_keep:
     column = data.get_col(data.headers.index(col))
     cleaned.append_col(column, header=col)
-  
+
   cleaned.headers = cols_to_keep
-  return cleaned.export('csv')
+  return cleaned
+
+def extract_emissions_share(data):
+  year = find_latest_emissions_year(data)
+  print year
+  total_for_latest = total_emissions(data, year)
+
+  emissions = extract_emissions(data)
+  share = []
+  for row in emissions:
+    if row[-1]:
+      share.append(float(row[-1]) / total_for_latest)
+    else:
+      share.append(0)
+  emissions.append_col(share, header='Emissions Share')
+
+  return emissions
+
 
 def find_latest_emissions_year(data):
   years = sorted([int(header) for header in data.headers if len(header) == 4])
@@ -23,17 +40,20 @@ def find_latest_emissions_year(data):
       return year
 
 def total_emissions(data, year):
-  column = data.get_col(data.headers.index(str(year)))
-  float_col = [float(amount) if amount else 0 for amount in column]
-  return sum(float_col)
+  year_column = data.headers.index(str(year))
+  code_column = data.headers.index('Country Code')
+  for country in data:
+    if country[code_column] == 'WLD' and country[year_column]:
+      if country[year_column]:
+        return float(country[year_column])
+  return 0
 
 if __name__ == '__main__':
   if sys.argv[-1].endswith('csv'):
     target = sys.argv[-1]
     dataset = tablib.Dataset().load(open(target).read())
-    
-    print extract_emissions(dataset)
-    # print find_latest_emissions_year(dataset)
-    # print total_emissions(dataset, 2014)
+
+    emissions = extract_emissions_share(dataset)
+    print emissions.export('csv')
   else:
     print 'call with a csv file as the only argument'
