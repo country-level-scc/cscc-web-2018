d3323f4fe6fd8d8cd524512ff96a82657764ca6e
Marcos Ojeda <subliminal@gmail.com>
2019-08-17 11:49:59 -0700

    Updates

---
+++ precache-manifest.3261a27c57bd6e548ce8b288e0702fb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554f9434fb2875ede81a8247f0a89501",
+    "url": "/cscc-web-2018/index.html"
+  },
+  {
+    "revision": "291fe87d8314b00dbfd3",
+    "url": "/cscc-web-2018/static/css/main.9d5f3a74.chunk.css"
+  },
+  {
+    "revision": "027d6edd00eafa32174a",
+    "url": "/cscc-web-2018/static/js/2.80f77940.chunk.js"
+  },
+  {
+    "revision": "291fe87d8314b00dbfd3",
+    "url": "/cscc-web-2018/static/js/main.626b11ab.chunk.js"
+  },
+  {
+    "revision": "4b6132166b96e0ae5b8a",
+    "url": "/cscc-web-2018/static/js/runtime~main.ee7ccc11.js"
+  }
+]);
